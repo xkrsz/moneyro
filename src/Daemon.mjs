7d6af24b6d9f48a551ee8b7d224e651e2b5e4f3f
Krzysztof Kraszewski <krzysztof@kraszew.ski>
2018-04-30 22:13:23 +0200

    feat: getBlockCount(), onGetBlockHash(), getBlockTemplate() implementation and docs

--- src/Daemon.mjs
+++ src/Daemon.mjs
@@ -1,6 +1,7 @@
 'use strict';
 
 import RPC from './RPC';
+import * as methods from './daemonMethods';
 
 export default class Daemon extends RPC {
 
@@ -10,6 +11,57 @@ export default class Daemon extends RPC {
   }) {
 
     super({ hostname, port });
+  }
+
+  /**
+   * @typedef {Object} BaseRPCResponse
+   * @property {string} status
+   */
+
+  /**
+   *
+   * @typedef {BaseRPCResponse} GetBlockCountResponse
+   * @property {int} count
+   *
+   * @returns {Promise<GetBlockCountResponse>}
+   */
+  async getBlockCount() {
+
+    return await this._request(methods.GET_BLOCK_COUNT);
+  }
+
+  /**
+   *
+   * @param {int} blockHeight
+   *
+   * @returns {Promise<string>} - block hash
+   */
+  async onGetBlockHash(blockHeight) {
+
+    return await this._request(methods.ON_GET_BLOCK_HASH, [blockHeight]);
+  }
+
+  /**
+   *
+   * @typedef {BaseRPCResponse} GetBlockTemplateResponse
+   * @property {string} blocktemplateBlob
+   * @property {uint} difficulty
+   * @property {uint} height
+   * @property {string} prevHash
+   * @property {uint} reservedOffset
+   *
+   *
+   * @param {string} walletAddress
+   * @param {uint} reserveSize
+   *
+   * @returns {Promise<GetBlockTemplateResponse>}
+   */
+  async getBlockTemplate(walletAddress, reserveSize) {
+
+    return await this._request(methods.GET_BLOCK_TEMPLATE, {
+      wallet_address: walletAddress,
+      reserve_size: reserveSize,
+    });
   }
 
 }
