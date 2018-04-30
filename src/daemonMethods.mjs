330be3f153d48ece6c9840f0f2d64d72418c6a59
Krzysztof Kraszewski <krzysztof@kraszew.ski>
2018-04-30 22:13:55 +0200

    feat: daemon methods constants

---
+++ src/daemonMethods.mjs
@@ -0,0 +1,18 @@
+export const GET_BLOCK_COUNT = 'getblockcount';
+export const ON_GET_BLOCK_HASH = 'on_getblockhash';
+export const GET_BLOCK_TEMPLATE = 'getblocktemplate';
+export const SUBMIT_BLOCK = 'submitblock';
+export const GET_LAST_BLOCK_HEADER = 'getlastblockheader';
+export const GET_BLOCK_HEADER_BY_HASH = 'getblockheaderbyhash';
+export const GET_BLOCK_HEADER_BY_HEIGHT = 'getblockheaderbyheight';
+export const GET_BLOCK = 'getblock';
+export const GET_CONNECTIONS = 'get_connections';
+export const GET_INFO = 'get_info';
+export const HARD_FORK_INFO = 'HARD_FORK_INFO';
+export const SET_BANS = 'setbans';
+export const GET_BANS = 'getbans';
+
+// // other Daemon RPC calls
+// export const GET_TRANSACTIONS = 'gettransactions';
+// export const IS_KEY_IMAGE_SPENT = 'is_key_image_spent';
+//
